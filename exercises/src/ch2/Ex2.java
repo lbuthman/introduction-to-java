52e05fbe7baca0af507405fa8a6361e383f073a9
lbuthman <lbuthman@gmail.com>
2017-07-01 17:39:16 -0500

    add getting multiple inputs separated by space from Scanner

---
+++ exercises/src/ch2/Ex2.java
@@ -0,0 +1,32 @@
+package ch2;
+
+import tools.*;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/**
+ * Compute the volume of a cylinder
+ */
+
+public class Ex2 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        DecimalFormat dfShort = new DecimalFormat("0.0");
+        DecimalFormat dfLong = new DecimalFormat("0.0000");
+
+        Tools.print("Enter the radius and length of a cylinder: ");
+
+        //String arg = input.next();
+
+        double radius = input.nextDouble();
+        double length = input.nextDouble();
+
+        double area = radius * radius * Math.PI;
+        double volume = area * length;
+
+        Tools.println("The area is " + dfLong.format(area));
+        Tools.println("The volume is " + dfShort.format(volume));
+    }
+}
--- exercises/src/tools/Tools.java
+++ exercises/src/tools/Tools.java
@@ -6,6 +6,10 @@ package tools;
 public class Tools {
 
     public static void print(String s) {
+        System.out.print(s);
+    }
+
+    public static void println(String s) {
         System.out.println(s);
     }
 }
